1a328921fe2cc15b7971b62ffceab9877b8b5f41
Izaak13 <144282458+Izaak13@users.noreply.github.com>
2024-04-30 14:51:09 -0600

    commit

--- 01 Barebones Arduino Setup/Blink/Blink.ino
+++ 01 Barebones Arduino Setup/Blink/Blink.ino
@@ -22,21 +22,20 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
 //Global Variables
-int blinkTime=1000; //Parameter: milliseconds 
+int blinkTime=1000; //Parameter: milliseconds
 //Change this value to ensure new program is being uploaded
 //
-//const int led = 14; //Exemplar instead of key variable
-//
+//Const int led=14; //Exemplar instead of key variable
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT); //Hoverover to inspect parameters
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(blinkTime);                 // wait
+  delay(blinkTime);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(blinkTime);                 // wait
+  delay(blinkTime);                      // wait for a second
 }
